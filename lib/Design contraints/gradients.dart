e88b6aadbfe6dd7292aecaa2490c2ea819f2f66e
kirtihoodaa <kirtihooda0033@gmail.com>
2025-04-06 16:04:07 +0530

    gradient

---
+++ lib/Design contraints/gradients.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppGradients {
+  static  LinearGradient yellowOrangeVertical = LinearGradient(
+    colors: [
+      Color(0xFFFACC15),
+      Color(0xFFF97316),
+    ],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+}
